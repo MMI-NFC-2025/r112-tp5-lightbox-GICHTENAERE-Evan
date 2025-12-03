78e706a5b204e5c732931da46021c7e1d82e18f4
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2025-12-03 14:47:12 +0100

    commit tp

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-// *** LIGHTBOX ***
+const lightBox = document.querySelector("#lightbox");
+const images = document.querySelectorAll("img"); // ou la classe/sélecteur de vos images
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        lightBox.showModal();
+    });
+});
+
+// Pour fermer la lightbox
+lightBox.addEventListener("click", () => {
+    lightBox.close();
+});
